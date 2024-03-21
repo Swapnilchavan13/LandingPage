bef35846cbe2c885216ff8177471b57ff6c56d71
swapnil chavan <chavanswapnil822@gmail.com>
2024-03-21 15:43:14 +0530

    updated

---
+++ mylanding/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { RegistrationForm } from './Component/RegistrationForm';
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
