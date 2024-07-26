2c2f8ea4f3146874fdcd16f64cdd63f4809d7bd8
swapnil chavan <chavanswapnil822@gmail.com>
2024-07-26 16:18:42 +0530

    updated

--- mylanding/src/Component/Firstformdata.jsx
+++ mylanding/src/Component/Firstformdata.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/firstformdata.css'; // Import the CSS file
 
 export const Firstformdata = () => {
   const [merchants, setMerchants] = useState([]);
@@ -24,117 +25,118 @@ export const Firstformdata = () => {
   }, [search, merchants]);
 
   return (
-    <div style={styles.container}>
+    <div id="container">
+      <h1>First Form Data (Merchant)</h1>
       <input
         type="text"
         placeholder="Search by mobile number"
         value={search}
         onChange={e => setSearch(e.target.value)}
-        style={styles.searchInput}
+        id="searchInput"
       />
-      <div style={styles.merchantCarddiv}>
+      <div id="merchantCarddiv">
         {filteredMerchants.map(merchant => (
-          <div key={merchant._id} style={styles.merchantCard}>
+          <div key={merchant._id} id="merchantCard">
             <h2>{merchant.businessName}</h2>
-            <div style={styles.field}>
-              <label style={styles.label}>Business Type:</label>
+            <div id="field">
+              <label id="label">Business Type:</label>
               <p>{merchant.businessType}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Business Address:</label>
+            <div id="field">
+              <label id="label">Business Address:</label>
               <p>{merchant.businessAddress}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Contact Email:</label>
+            <div id="field">
+              <label id="label">Contact Email:</label>
               <p>{merchant.contactEmail}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Contact Phone Number:</label>
+            <div id="field">
+              <label id="label">Contact Phone Number:</label>
               <p>{merchant.contactPhoneNumber}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Person Name:</label>
+            <div id="field">
+              <label id="label">Person Name:</label>
               <p>{merchant.personName} {merchant.lastName}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Username:</label>
+            <div id="field">
+              <label id="label">Username:</label>
               <p>{merchant.username}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Password:</label>
+            <div id="field">
+              <label id="label">Password:</label>
               <p>{merchant.password}</p>
             </div>
             {merchant.profileImage && (
-              <div style={styles.field}>
-                <label style={styles.label}>Profile Image/Video:</label>
+              <div id="field">
+                <label id="label">Profile Image/Video:</label>
                 {merchant.profileImage.endsWith('.mp4') || merchant.profileImage.endsWith('.avi') || merchant.profileImage.endsWith('.mov') || merchant.profileImage.endsWith('.mkv') ? (
-                  <video controls width="250" style={styles.media}>
+                  <video controls width="250" id="media">
                     <source src={`https://localitebackend.localite.services/${merchant.profileImage}`} type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
                 ) : (
-                  <img src={`https://localitebackend.localite.services/${merchant.profileImage}`} alt={merchant.businessName} style={styles.media} />
+                  <img src={`https://localitebackend.localite.services/${merchant.profileImage}`} alt={merchant.businessName} id="media" />
                 )}
               </div>
             )}
-            <div style={styles.field}>
-              <label style={styles.label}>Brand Logo:</label>
-              <img src={`https://localitebackend.localite.services/${merchant.brandLogo}`} alt={`${merchant.businessName} logo`} style={styles.media} />
+            <div id="field">
+              <label id="label">Brand Logo:</label>
+              <img src={`https://localitebackend.localite.services/${merchant.brandLogo}`} alt={`${merchant.businessName} logo`} id="media" />
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Business License:</label>
+            <div id="field">
+              <label id="label">Business License:</label>
               <p>{merchant.businessLicense}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>GST Certificate:</label>
+            <div id="field">
+              <label id="label">GST Certificate:</label>
               <p>{merchant.gstCertificate}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>PAN Card:</label>
+            <div id="field">
+              <label id="label">PAN Card:</label>
               <p>{merchant.panCard}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Proof of Address:</label>
+            <div id="field">
+              <label id="label">Proof of Address:</label>
               <p>{merchant.proofOfAddress}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Operation Hours:</label>
+            <div id="field">
+              <label id="label">Operation Hours:</label>
               <p>{merchant.operationHours}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Years of Business:</label>
+            <div id="field">
+              <label id="label">Years of Business:</label>
               <p>{merchant.yearsOfBusiness}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Number of Employees:</label>
+            <div id="field">
+              <label id="label">Number of Employees:</label>
               <p>{merchant.numberOfEmployees}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Product Description:</label>
+            <div id="field">
+              <label id="label">Brand Description:</label>
               <p>{merchant.productDescription}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Preferred Categories:</label>
+            <div id="field">
+              <label id="label">Preferred Categories:</label>
               <p>{merchant.preferredCategories}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Offer Frequency:</label>
+            <div id="field">
+              <label id="label">Offer Frequency:</label>
               <p>{merchant.offerFrequency}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Specific Requirements:</label>
+            <div id="field">
+              <label id="label">Specific Requirements:</label>
               <p>{merchant.specificRequirements}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>PAN/TAN Number:</label>
+            <div id="field">
+              <label id="label">PAN/TAN Number:</label>
               <p>{merchant.panTanNumber}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>GSTIN:</label>
+            <div id="field">
+              <label id="label">GSTIN:</label>
               <p>{merchant.gstin}</p>
             </div>
-            <div style={styles.field}>
-              <label style={styles.label}>Bank Account Details:</label>
+            <div id="field">
+              <label id="label">Bank Account Details:</label>
               <p>{merchant.bankAccountDetails}</p>
             </div>
           </div>
@@ -142,40 +144,4 @@ export const Firstformdata = () => {
       </div>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    padding: '20px',
-  },
-  searchInput: {
-    padding: '10px',
-    width: '60%',
-    marginBottom: '20px',
-    fontSize: '16px',
-  },
-  merchantCarddiv: {
-    display: 'grid',
-    gridtemplatecolumn: 'repeat(4, 1fr)',
-    gap: '20px',
-  },
-  merchantCard: {
-    border: '1px solid #ddd',
-    borderRadius: '8px',
-    padding: '20px',
-    marginBottom: '20px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-  },
-  field: {
-    marginBottom: '40px',
-  },
-  label: {
-    fontWeight: 'bold',
-  },
-  media: {
-    width: '250px',
-    height: 'auto',
-    display: 'block',
-    margin: '10px 0',
-  },
 };
